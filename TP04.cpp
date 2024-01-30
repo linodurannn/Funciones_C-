6c752ba3578281c5251773df96f86ba711b2420d
linodurannn <155786462+linodurannn@users.noreply.github.com>
2024-01-29 21:49:35 -0400

    TP04.cpp

---
+++ TP04.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+string nombres[] = {"Ervin","Diego","Andrea"};
+string apellidos[] = {"Jugo","Ensalada","Frape","Tarta","Helado","Mermelada","Batido","Asado","Coctel","Crudo"};
+string contrasenas[] = {}
+string notas[] = {}
